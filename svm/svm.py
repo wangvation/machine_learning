e4c6d0fac366fec0aae16c59d56b09a79f24ea15
wangvation <wangvation@gmail.com>
2017-11-01 23:52:59 +0800

    add neural_network.md

--- svm/svm.py
+++ svm/svm.py
@@ -25,6 +25,7 @@ class svm(object):
         self.label_mat = np.mat(label_set).T
         self.data_size = len(label_set)
         self.kernel_mat = self.kernel_trans(self.data_mat, self.data_mat)
+        self.errors = [0 for _ in range(self.data_size)]
         self.smo()
         return self
 
@@ -86,27 +87,33 @@ class svm(object):
         eta = self.kernel_mat[index1, index1] +\
             self.kernel_mat[index2, index2] -\
             2.0 * self.kernel_mat[index1, index2]
-
+        if eta <= 0:
+            return 0
         alpha2_new = alpha2_old + label2 * (error1 - error2) / eta
         L, H = self.get_bounds(alpha1_old, alpha2_old, label1, label2)
         if L == H:
             return 0
-        alpha2_new = self.clip_alpha(alpha2_new, L, H)
-        if abs(alpha2_new - alpha2_old) < 0.00001:
+        alpha2_clip = self.clip_alpha(alpha2_new, L, H)
+        if abs(alpha2_clip - alpha2_old) < 0.00001:
             return 0
-        self.alphas[index2] = alpha2_new
-        alpha1_new = alpha1_old + label1 * label2 * (alpha2_old - alpha2_new)
+        self.alphas[index2] = alpha2_clip
+        alpha1_new = alpha1_old + label1 * \
+            label2 * (alpha2_old - alpha2_clip)
         self.alphas[index1] = alpha1_new
 
-        b1_new = self.bias - error1 - label1 * \
-            (alpha1_new - alpha1_old) * self.kernel_mat[index1, index1] - \
-            label2 * (alpha2_new - alpha2_old) * self.kernel_mat[index1, index2]
+        b1_new = self.bias + error1 - label1 * \
+            (alpha1_new - alpha1_old) * self.kernel_mat[index1, index1] + \
+            label2 * (alpha2_clip - alpha2_old) * \
+            self.kernel_mat[index1, index2]
 
-        b2_new = self.bias - error2 - label1 * \
-            (alpha1_new - alpha1_old) * self.kernel_mat[index1, index2] - \
-            label2 * (alpha2_new - alpha2_old) * self.kernel_mat[index2, index2]
+        b2_new = self.bias + error2 - label1 * \
+            (alpha1_new - alpha1_old) * self.kernel_mat[index1, index2] + \
+            label2 * (alpha2_clip - alpha2_old) * \
+            self.kernel_mat[index2, index2]
 
         self.bias = self.get_new_bias(b1_new, b2_new, alpha1_new, alpha2_new)
+        self.update_errors(index1)
+        self.update_errors(index2)
         return 1
 
     def is_meet_KKT(self, index):
@@ -118,24 +125,25 @@ class svm(object):
         if func_dist >= 1:
             return self.alphas[index] == 0
 
-    def select_second_alpha(self, first_index, valid_indexes):
-        max_delta = 0
+    def select_second_alpha(self, first_index,
+                            valid_indexes, debug=False):
+        max_error = 0
         second_index = -1
         error_1 = self.errors[first_index]
         for j in valid_indexes:
-            if j is not first_index \
-                    and 0 < self.alphas[j] < self.C and self.is_meet_KKT(j):
+            if j is not first_index:
                 error_j = self.errors[j]
-                delta = abs(error_1 - error_j)
-                if delta > max_delta:
-                    max_delta = delta
+                error = abs(error_1 - error_j)
+                # if debug:
+                #     print(error)
+                if error >= max_error:
+                    max_error = error
                     second_index = j
         return second_index
 
-    def update_errors(self):
-        self.errors = (np.dot(np.multiply(self.alphas, self.label_mat).T,
-                              self.kernel_mat) + self.bias).T - self.label_mat
-        return self.errors
+    def update_errors(self, index):
+        error = self.predict(index) - self.label_mat[index]
+        self.errors[index] = float(error)
 
     def smo(self):
         self.bias = 0.0
@@ -143,8 +151,9 @@ class svm(object):
         _iter = 0
         entier_flag = True
         update_num = 0
-        self.update_errors()
-
+        for i in range(self.data_size):
+            self.update_errors(i)
+        debug = True
         while (_iter < self.max_iter and update_num > 0) or entier_flag:
             update_num = 0
             if entier_flag:
@@ -152,22 +161,25 @@ class svm(object):
                 for i in valid_indexes:
                     if self.is_meet_KKT(i):
                         continue
-                    index_2 = self.select_second_alpha(i, valid_indexes)
-                    if index_2 is -1:
+                    index2 = self.select_second_alpha(i, valid_indexes, False)
+                    if index2 is -1:
                         continue
-                    update_num += self.update_alpha_bias(i, index_2)
+                    update_num += self.update_alpha_bias(i, index2)
                 entier_flag = False
             else:
                 valid_indexes = np.nonzero(np.multiply(
-                    self.alphas < self.C, self.alphas > 0))[1]
+                    self.alphas < self.C, self.alphas > 0))[0]
+                if debug:
+                    debug = False
+                    print(valid_indexes)
                 for i in valid_indexes:
-                    index_2 = self.select_second_alpha(
-                        i, valid_indexes)
-                    if index_2 is -1:
+                    index2 = self.select_second_alpha(i, valid_indexes, True)
+                    if index2 is -1:
                         continue
-                    update_num += self.update_alpha_bias(i, index_2)
+                    update_num += self.update_alpha_bias(i, index2)
 
-            self.update_errors()
+            # if _iter % 100 == 0:
+            #     print(np.sum(np.array(self.errors)**2))
             _iter += 1
         self.get_support_vec()
 
@@ -192,67 +204,37 @@ class svm(object):
 
 
 if __name__ == '__main__':
-    data = pd.read_csv('../dataset/iris/bezdekIris.data')
-    col_list = ['sepal_length', 'sepal_width',
-                'petal_length', 'petal_width', 'Class']
-
-    setosa_versicolor = data[data.Class != 'Iris-virginica']
-    setosa_virginica = data[data.Class != 'Iris-versicolor']
-    versicolor_virginica = data[data.Class != 'Iris-setosa']
-    setosa_versicolor.iloc[:, -1] = \
-        setosa_versicolor.iloc[:, -1].apply(
-        lambda x: 1 if x == 'Iris-versicolor' else -1)
-    setosa_virginica.iloc[:, -1] = \
-        setosa_virginica.iloc[:, -1].apply(
-        lambda x: 1 if x == 'Iris-virginica' else -1)
-    versicolor_virginica.iloc[:, -1] = \
-        versicolor_virginica.iloc[:, -1].apply(
-        lambda x: 1 if x == 'Iris-versicolor' else -1)
-
-    result_dict = {"setosa_versicolor": {-1: "Iris-setosa",
-                                         1: "Iris-versicolor"},
-                   "setosa_virginica": {-1: "Iris-setosa",
-                                        1: "Iris-virginica"},
-                   "versicolor_virginica": {-1: "Iris-virginica",
-                                            1: "Iris-versicolor"}
-                   }
-
-    svm_setosa_versicolor = svm(C=1.0, max_iter=1000000,
-                                func='rbf', sigma=0.7)\
-        .fit(setosa_versicolor.iloc[:, :-1].values,
-             setosa_versicolor.iloc[:, -1].values,)
-    svm_setosa_virginica = svm(C=1.0, max_iter=1000000,
-                               func='rbf', sigma=0.7)\
-        .fit(setosa_virginica.iloc[:, :-1].values,
-             setosa_virginica.iloc[:, -1].values)
-    svm_versicolor_virginica = svm(C=1.0, max_iter=1000000,
-                                   func='rbf', sigma=0.7)\
-        .fit(versicolor_virginica.iloc[:, :-1].values,
-             versicolor_virginica.iloc[:, -1].values)
-
-    svms = {"setosa_versicolor": svm_setosa_versicolor,
-            "setosa_virginica": svm_setosa_virginica,
-            "versicolor_virginica": svm_versicolor_virginica}
-    test_index = [random.randint(0, 149) for _ in range(20)]
-    right_num = 0
-    for index in test_index:
-        class_dict = {
-            'Iris-setosa': 0,
-            'Iris-virginica': 0,
-            'Iris-versicolor': 0
-        }
-        test_data = data.iloc[index]
-        label = None
-        for name, _svm in svms.items():
-            result_label = _svm.classifier(test_data.iloc[:-1].values)
-            result_class = result_dict[name][result_label]
-            class_dict[result_class] += 1
-        max_vote = 0
-        for _class, vote in class_dict.items():
-            if vote > max_vote:
-                max_vote = vote
-                label = _class
-        if label == test_data.iloc[-1]:
-            right_num += 1
-        # print(class_dict)
-    print(str(right_num) + '/' + str(len(test_index)))
+    data = pd.read_csv(
+        '../dataset/breast-cancer-wisconsin/breast-cancer-wisconsin.data')
+    col_list = ['id', 'Clump_Thickness', 'Uniformity_of_Cell_Size',
+                'Uniformityof_Cell_Shape', 'Marginal_Adhesion',
+                'Single_Epithelial', 'Bare_Nuclei', 'Bland_Chromatin',
+                'Normal_Nucleoli', 'Mitoses', 'Class']
+    # remove the abnormal data
+    data = data[data.Bare_Nuclei != '?']
+    data.iloc[:, 6] = data.iloc[:, 6].apply(lambda x: float(x))
+    # 0 for benign, 1 for malignant
+    data[col_list[-1]] = data[col_list[-1]].apply(lambda x: 1 if x == 4 else -1)
+    data_size = len(data.values)
+    data_index = range(data_size)
+    # k - fold CrossValidation
+    errors = 0
+    rights = 0
+    test_index = [random.randint(0, data_size - 1)
+                  for _ in range(data_size / 10)]
+    train_index = list(set(data_index) ^ set(test_index))
+    test_set = data[col_list[1:-1]].iloc[test_index].values
+    test_label = data[col_list[-1]].iloc[test_index].values
+    train_set = data[col_list[1:-1]].iloc[train_index].values
+    label_set = data[col_list[-1]].iloc[train_index].values
+    # classifier = svm(C=100, max_iter=10000, func='rbf', sigma=0.9)
+    classifier = svm(C=100, max_iter=10000, func='linear')
+    classifier.fit(train_set, label_set)
+    for i in range(len(test_label)):
+        result_label = classifier.classifier(test_set[i])
+        print(result_label)
+        if result_label != test_label[i]:
+            errors += 1
+        else:
+            rights += 1
+    print([rights, errors])
