3b14d21d37a72cb09f873016c5c189b7d4f208c3
wangvation <wangvation@gmail.com>
2017-12-10 10:58:16 +0800

    fix file error on conv_kernel

--- cnn/cnn_utils.py
+++ cnn/cnn_utils.py
@@ -4,69 +4,70 @@ import numpy as np
 
 
 def around_with_zero(input_array, width_padding, height_padding):
-    """
+  """
 
-    Args:
-        input_array:
-        width_padding:
-        height_padding:
+  Args:
+      input_array:
+      width_padding:
+      height_padding:
 
-    Returns:
+  Returns:
 
-    """
-    if input_array.ndim == 2:
-        height, width = input_array.shape
-        new_array = np.zeros((height + 2 * height_padding,
-                              width + 2 * width_padding), dtype=np.float32)
-        new_array[height_padding:-height_padding,
-                  width_padding:-width_padding] = input_array
-        return new_array
-    else:
-        depth, height, width = input_array.shape
-        new_array = np.zeros((depth, height + 2 * height_padding,
-                              width + 2 * width_padding), dtype=np.float32)
-        new_array[:, height_padding:-height_padding,
-                  width_padding:-width_padding] = input_array
-        return new_array
+  """
+  print("sss", input_array.shape)
+  if input_array.ndim == 2:
+    height, width = input_array.shape
+    new_array = np.zeros((height + 2 * height_padding,
+                          width + 2 * width_padding), dtype=np.float32)
+    new_array[height_padding:-height_padding,
+              width_padding:-width_padding] = input_array
+    return new_array
+  else:
+    depth, height, width = input_array.shape
+    new_array = np.zeros((depth, height + 2 * height_padding,
+                          width + 2 * width_padding), dtype=np.float32)
+    new_array[:, height_padding:-height_padding,
+              width_padding:-width_padding] = input_array
+    return new_array
 
 
 def get_patch(i, j, array, kernel_shape, stride):
-    """
+  """
 
-    Args:
-      i: row index
-      j: col index
-      array:
-      kernel_shape:
+  Args:
+    i: row index
+    j: col index
+    array:
+    kernel_shape:
 
-    Returns:
+  Returns:
 
-    """
-    depth, width, height = expand_shape(kernel_shape)
-    start_i = i * stride
-    start_j = j * stride
+  """
+  depth, width, height = expand_shape(kernel_shape)
+  start_i = i * stride
+  start_j = j * stride
 
-    if array.ndim == 2:
-        return array[start_i:start_i + height,
-                     start_j:start_j + width]
-    if array.ndim == 3:
-        return array[:, start_i:start_i + height,
-                     start_j:start_j + width]
+  if array.ndim == 2:
+    return array[start_i:start_i + height,
+                 start_j:start_j + width]
+  if array.ndim == 3:
+    return array[:, start_i:start_i + height,
+                 start_j:start_j + width]
 
 
 def expand_shape(shape, default_depth=None):
-    """
-    expand the shape
-    Args:
-      shape:
+  """
+  expand the shape
+  Args:
+    shape:
 
-    Returns:
-    depth, height, width
-    if the length of shape is 2, depth is None,
-    """
-    if len(shape) == 3:
-        depth, height, width = shape
-    else:
-        depth = default_depth
-        height, width = shape
-    return depth, height, width
+  Returns:
+  depth, height, width
+  if the length of shape is 2, depth is None,
+  """
+  if len(shape) == 3:
+    depth, height, width = shape
+  else:
+    depth = default_depth
+    height, width = shape
+  return depth, height, width
